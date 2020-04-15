5161c6bcdb54d7d98c11478860d1df63026a61eb
Marco André Pereira da Costa <43896022+Mapc1@users.noreply.github.com>
2020-04-15 01:13:49 +0100

    gpuswitch

---
+++ gpuswitch/intel.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+
+cp -p /home/marco/.gpuswitch/intel /etc/X11/xorg.conf.d/nvidia.conf
+
+cp -p /home/marco/.gpuswitch/grub/intelgrub /etc/default/grub
---
+++ gpuswitch/nvidia.sh
@@ -0,0 +1,5 @@
+#/usr/bin/bash
+
+cp -p /home/marco/.gpuswitch/nvidia /etc/X11/xorg.conf.d/nvidia.conf
+
+cp -p /home/marco/.gpuswitch/grub/nvidiagrub /etc/default/grub
