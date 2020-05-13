b0c0084cdc5b7de9d92a0f2dabbdd88e1fccf6ad
Marco Costa <marco@localhost-live.home>
2020-05-13 14:41:39 +0100

    Add sudo to nvidia.sh and intel.sh

--- gpuswitch/intel.sh
+++ gpuswitch/intel.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 
-cp -p /home/marco/.gpuswitch/intel /etc/X11/xorg.conf.d/nvidia.conf
+sudo cp -p /home/marco/.gpuswitch/intel /etc/X11/xorg.conf.d/nvidia.conf
 
-cp -p /home/marco/.gpuswitch/grub/intelgrub /etc/default/grub
+sudo cp -p /home/marco/.gpuswitch/grub/intelgrub /etc/default/grub
