78e402f4a72279dee5d5c7ee735f6f769e8cc291
Mapc1 <marcocosta579@gmail.com>
2021-03-16 16:43:01 +0000

    Update

--- monitors.sh
+++ monitors.sh
@@ -1,5 +1,6 @@
-if xrandr -q | grep 'HDMI-0 connected'; then
-	xrandr --output HDMI-1-0 --right-of eDP-1
+if xrandr -q | grep 'HDMI-1-0 connected'; then
+	xrandr --output HDMI-1-0 --auto
+    xrandr --output HDMI-1-0 --right-of eDP-1
 else
 	xrandr --output eDP-1 --auto
 fi
